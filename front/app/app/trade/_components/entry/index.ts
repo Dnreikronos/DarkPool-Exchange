67f64df355b6bebe0dca937eb1de39e2c4e162c3
yuribodo <yemotalara@gmail.com>
2026-06-01 07:42:20 -0300

    chore(#99): export real-submission builders, error mapper, SubmitError

--- front/app/app/trade/_components/entry/index.ts
+++ front/app/app/trade/_components/entry/index.ts
@@ -2,13 +2,16 @@ export { OrderEntry, type OrderEntryHandle, type OrderEntryProps } from './Order
 export { BuySellTabs } from './BuySellTabs'
 export { DecimalInput } from './inputs'
 export { TotalRow } from './TotalRow'
-export { PlaceButton } from './ProveSubmitStages'
+export { PlaceButton, SubmitError } from './ProveSubmitStages'
 export { useOrderForm } from '../../_hooks/entry/useOrderForm'
+export { useRealSubmission } from '../../_hooks/entry/useRealSubmission'
 export {
   useSubmitStages,
   runSubmission,
+  buildMockSteps,
   type SubmissionPhase,
   type SubmitPayload,
+  type StageStep,
 } from '../../_hooks/entry/useSubmitStages'
 export {
   validateOrder,
@@ -27,7 +30,22 @@ export {
   STAGE_ORDER,
   STAGE_TOTAL_MS,
   SUCCESS_HOLD_MS,
+  ORDER_PAIR,
+  ORDER_TTL_NS,
   type SubmitStageId,
 } from '../../_lib/entry/policy'
 export { errorMessage } from '../../_lib/entry/errors'
 export { computeFee, computeGrandTotal, computeTotal } from '../../_lib/entry/derive'
+export {
+  buildOrderPayload,
+  buildWitness,
+  createRealSteps,
+  randomHex,
+  type RealStepDeps,
+} from '../../_lib/entry/build-submission'
+export {
+  mapSubmissionError,
+  submitErrorMessage,
+  toSubmitErrorDetail,
+  type SubmitErrorDetail,
+} from '../../_lib/entry/submit-error'
